45775ba6bc465bcd1121dd25b4e68dc214ae138e
Lee Kin Fei <kinfei.lee@qumonintelligence.com>
2021-08-06 22:33:18 +0800

    Updates

---
+++ precache-manifest.3c1d7bf24c7db2138b0aaabc5ec4cb66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d036e73ed9ab047a860194196c06bd5",
+    "url": "/timezone-selections/index.html"
+  },
+  {
+    "revision": "4db8d8af2e67c4fcc2cf",
+    "url": "/timezone-selections/static/css/2.35fcc229.chunk.css"
+  },
+  {
+    "revision": "6c863ed2b1776f901493",
+    "url": "/timezone-selections/static/css/main.652a9ee7.chunk.css"
+  },
+  {
+    "revision": "4db8d8af2e67c4fcc2cf",
+    "url": "/timezone-selections/static/js/2.1e0ec42a.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/timezone-selections/static/js/2.1e0ec42a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c863ed2b1776f901493",
+    "url": "/timezone-selections/static/js/main.ef70c506.chunk.js"
+  },
+  {
+    "revision": "688c42c6006dc66153cc",
+    "url": "/timezone-selections/static/js/runtime-main.f14e7e25.js"
+  }
+]);
